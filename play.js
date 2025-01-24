1dee02948d53e7c8dce6b53d4690deb50835e0b9
Anika Nawar <nawarani47@gmail.com>
2025-01-24 02:55:46 -0500

    separated connection function

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const net = require("net");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host : "localhost",
-    port: 50541
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  conn.setEncoding("utf8");
-  return conn;
-};
+const connect = require("./client.js");
 
 console.log("Connecting ...");
 connect();
